6eb066f7211a286aaa43224a6b9b10b36dc279e3
Matheus Neves <mtheus@users.noreply.github.com>
2020-09-24 13:06:22 -0300

    Update main.go
    POC

--- main.go
+++ main.go
@@ -6,8 +6,27 @@ import (
 
 func main() {
 	fmt.Println(hello())
+	openBrowser("https://www.google.com/search?q=ufu")
 }
 
 func hello() string {
 	return "hello"
+}
+
+func openBrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
 }
